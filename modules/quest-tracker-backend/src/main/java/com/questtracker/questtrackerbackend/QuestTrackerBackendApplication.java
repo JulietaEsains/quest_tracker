431db46abddc7595ed3d281cc57878cb208242e8
JulietaEsains <jesains@gmail.com>
2022-12-23 15:32:26 -0300

    repositorio inicializado

---
+++ modules/quest-tracker-backend/src/main/java/com/questtracker/questtrackerbackend/QuestTrackerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.questtracker.questtrackerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestTrackerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestTrackerBackendApplication.class, args);
+	}
+
+}
---
+++ modules/quest-tracker-backend/src/test/java/com/questtracker/questtrackerbackend/QuestTrackerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.questtracker.questtrackerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestTrackerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
