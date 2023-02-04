a0a2dcd93ca3566f342155e4d39bfc226690d361
JulietaEsains <jesains@gmail.com>
2023-02-04 18:06:13 -0300

    simplificación de uno de los métodos del servicio de usuario

--- modules/quest-tracker-backend/src/main/java/com/quest_tracker/quest_tracker_backend/services/UserService.java
+++ modules/quest-tracker-backend/src/main/java/com/quest_tracker/quest_tracker_backend/services/UserService.java
@@ -27,16 +27,12 @@ public class UserService extends GenericService<User> implements UserTransaction
     public Double findLevelPercentage(Long id) throws Exception {
         var opt = this.repository.findById(id);
         User user = opt.get();
-        if (opt.isPresent()) {
-            Double currentLevelExperience = 0.0;
-            if (user.getLevel() == 1L) {
-                currentLevelExperience = Double.valueOf(user.getCurrentExperience());
-            } else {
-                currentLevelExperience = Double.valueOf(user.getCurrentExperience() - 1000 * user.getLevel());
-            }
-            return (currentLevelExperience/1000) * 100;
+        Double currentLevelExperience = 0.0;
+        if (user.getLevel() == 1L) {
+            currentLevelExperience = Double.valueOf(user.getCurrentExperience());
         } else {
-            throw new Exception(String.format("%s with id %d not found in repository.", opt.getClass(), (Long) id));
+            currentLevelExperience = Double.valueOf(user.getCurrentExperience() - 1000 * user.getLevel());
         }
+        return (currentLevelExperience/1000) * 100;
     }
 }
